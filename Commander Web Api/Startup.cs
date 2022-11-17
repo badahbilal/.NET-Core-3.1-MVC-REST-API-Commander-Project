a1d53d297d67dbe7195f04951a6a417bf2617e15
BADAH Bilal <bilalbadah@gmail.com>
2022-11-17 20:53:35 +0100

    Commit 8 : Adding DataAnnotation to command model and Creating Migrations and the DataBase

--- Commander Web Api/Startup.cs
+++ Commander Web Api/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -31,6 +32,9 @@ namespace Commander_Web_Api
 
             // We Add Our Main Repo To our Dependency Container  
             services.AddScoped<ICommanderRepo, MockCommanderRepo>();
+
+
+            services.AddDbContext<CommanderContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
