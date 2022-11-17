720477f353ea1f57f3ef8084905cf5590e620d1f
BADAH Bilal <bilalbadah@gmail.com>
2022-11-17 18:00:17 +0100

    Commit number 04 :  Create Commands Controller and add to it  Annotations Of Api

---
+++ Commander Web Api/Controllers/CommandsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Commander_Web_Api.Controllers
+{
+    //api/commands
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        
+    }
+}
