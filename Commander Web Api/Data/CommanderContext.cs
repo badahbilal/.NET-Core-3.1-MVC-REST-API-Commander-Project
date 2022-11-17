826cbf85a03d39324bc034986b04a04df3c7454e
BADAH Bilal <bilalbadah@gmail.com>
2022-11-17 19:50:44 +0100

    Commit 07 : installing dependencies of EF Core and Creating Class of type DbContext to create our database

---
+++ Commander Web Api/Data/CommanderContext.cs
@@ -0,0 +1,16 @@
+﻿using Commander_Web_Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Commander_Web_Api.Data
+{
+    public class CommanderContext : DbContext
+    {
+        public CommanderContext(DbContextOptions<CommanderContext> options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<Command> Commands { get; set; }
+    }
+}
