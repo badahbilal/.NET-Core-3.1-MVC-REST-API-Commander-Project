bbb4e21c46ef28bb92f7600857ce292e86e5dfd8
BADAH Bilal <bilalbadah@gmail.com>
2022-11-17 17:53:04 +0100

    Commit 03 ==> we add a general repository Interface and Mock Commander Repo that implementing The Repository Inteface

---
+++ Commander Web Api/Data/ICommanderRepo.cs
@@ -0,0 +1,15 @@
+﻿using Commander_Web_Api.Models;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Commander_Web_Api.Data
+{
+    public interface ICommanderRepo
+    {
+        IEnumerable<Command> GetAppCommands();
+
+        Command GetCommandById(int id);
+
+
+    }
+}
---
+++ Commander Web Api/Data/MockCommanderRepo.cs
@@ -0,0 +1,24 @@
+﻿using Commander_Web_Api.Models;
+using System.Collections.Generic;
+
+namespace Commander_Web_Api.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command { Id = 0, HowTo = "Boil an egg", Line = "Boil Water", Plateform = "Kettle & Pan" },
+                new Command { Id = 1, HowTo = "cut Bread", Line = "Get a knife", Plateform = "Knife & chopping board" },
+                new Command { Id = 2, HowTo = "Make cup of tea", Line = "Place teabag in cup", Plateform = "Lettle & cup" }
+            };
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command { Id = 0, HowTo = "Boil an egg", Line = "Boil Water", Plateform = "Kettle & Pan" };
+        }
+    }
+}
