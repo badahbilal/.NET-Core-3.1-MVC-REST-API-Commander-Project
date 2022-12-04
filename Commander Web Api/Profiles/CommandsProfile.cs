ecfd6d4a22461f53be6da4509954389fc0d2a5f0
BADAH Bilal <bilalbadah@gmail.com>
2022-12-04 12:44:30 +0100

    Commit 12 : Create mapCommand  class to ma between a source object to destination object

---
+++ Commander Web Api/Profiles/CommandsProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Commander_Web_Api.Dtos;
+using Commander_Web_Api.Models;
+
+namespace Commander_Web_Api.Profiles
+{
+    public class CommandsProfile : Profile
+    {
+        public CommandsProfile()
+        {
+            CreateMap<Command, CommandReadDto>();
+        }
+    }
+}
